f8951f59067d4d5150f79a6a1a91ff51e46ca77a
Rahuls-iMac <rahwinside@gmail.com>
2020-09-21 15:47:17 +0530

    Masonry

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyProfile()));
+  runApp(MaterialApp(
+    home: MyProfile(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff333333),
       appBar: AppBar(
         title: Text("My Profile"),
       ),
@@ -50,7 +54,229 @@ class MyProfile extends StatelessWidget {
                     fit: BoxFit.fitWidth),
               ),
             ),
-            Container(),
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0, left: 15, right: 15),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Education",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          gradient: LinearGradient(
+                              colors: [Color(0xffe04a88), Color(0xff861f42)],
+                              end: Alignment.topLeft,
+                              begin: Alignment.bottomRight)),
+                      child: Text(
+                        "Bachelor of Technology, Information Technology",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          gradient: LinearGradient(
+                              colors: [Color(0xffe04a88), Color(0xff861f42)],
+                              end: Alignment.topLeft,
+                              begin: Alignment.bottomRight)),
+                      child: Text(
+                        "Bachelor of Arts, Hindi Language and Literature",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0, left: 15, right: 15),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Project Work",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Flexible(
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 5),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 10),
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    alignment: Alignment.centerLeft,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.all(Radius.circular(10)),
+                                        gradient: LinearGradient(
+                                            colors: [
+                                              Color(0xffe04a88),
+                                              Color(0xff861f42)
+                                            ],
+                                            end: Alignment.topLeft,
+                                            begin: Alignment.bottomRight)),
+                                    child: Text(
+                                      "Bachelor of Arts, Hindi Language and Literature",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 10),
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    alignment: Alignment.centerLeft,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.all(Radius.circular(10)),
+                                        gradient: LinearGradient(
+                                            colors: [
+                                              Color(0xffe04a88),
+                                              Color(0xff861f42)
+                                            ],
+                                            end: Alignment.topLeft,
+                                            begin: Alignment.bottomRight)),
+                                    child: Text(
+                                      "Bachelor of Arts, Hindi Language and Literature",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 5),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 10),
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    alignment: Alignment.centerLeft,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.all(Radius.circular(10)),
+                                        gradient: LinearGradient(
+                                            colors: [
+                                              Color(0xffe04a88),
+                                              Color(0xff861f42)
+                                            ],
+                                            end: Alignment.topLeft,
+                                            begin: Alignment.bottomRight)),
+                                    child: Text(
+                                      "Bachelor of Arts, Hindi Language and Literature",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 10),
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    alignment: Alignment.centerLeft,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.all(Radius.circular(10)),
+                                        gradient: LinearGradient(
+                                            colors: [
+                                              Color(0xffe04a88),
+                                              Color(0xff861f42)
+                                            ],
+                                            end: Alignment.topLeft,
+                                            begin: Alignment.bottomRight)),
+                                    child: Text(
+                                      "Bachelor of Arts, Hindi Language and Literature",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 10),
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    alignment: Alignment.centerLeft,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.all(Radius.circular(10)),
+                                        gradient: LinearGradient(
+                                            colors: [
+                                              Color(0xffe04a88),
+                                              Color(0xff861f42)
+                                            ],
+                                            end: Alignment.topLeft,
+                                            begin: Alignment.bottomRight)),
+                                    child: Text(
+                                      "Bachelor of Arts, Hindi Language and Literature",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
